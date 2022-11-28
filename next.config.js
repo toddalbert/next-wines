a8555f6aef312aa10caea16a6bed9bbfff821346
Todd Albert <todd@bocacode.com>
2022-11-28 14:55:56 -0500

    getting wines

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'images.vivino.com',
+      'thumbs.vivino.com',
+      'www.wine-searcher.com',
+      's.wine.style',
+    ],
+  },
 }
 
 module.exports = nextConfig
